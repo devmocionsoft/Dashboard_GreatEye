354439ea50a667977533e91f3852f86d0c55701d
Jeisson <jeissonssp@gmail.com>
2022-07-26 11:23:00 -0500

    Selector de imagenes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,45 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import {
-  ref,
-  uploadBytes,
-  getDownloadURL,
-  listAll,
-  list,
-} from "firebase/storage";
+import { ref, getDownloadURL, listAll } from "firebase/storage";
 import { storage } from "./firebase";
-import { v4 } from "uuid";
 
 function App() {
-  const [imageUpload, setImageUpload] = useState(null);
-  const [imageUrls, setImageUrls] = useState([]);
-
-  console.log(imageUrls)
-
-  const imagesListRef = ref(storage, "images/");
-  const uploadFile = () => {
-    if (imageUpload == null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImageUrls((prev) => [...prev, url]);
-      });
-    });
-  };
+  const [images, setImages] = useState([]);
+  const [show, setShow] = useState(false);
+  const [selected, setSelected] = useState();
 
   useEffect(() => {
+    const imagesListRef = ref(storage, "/");
     listAll(imagesListRef).then((response) => {
+      setImages([]);
       response.items.forEach((item) => {
+        const name = item.name;
         getDownloadURL(item).then((url) => {
-          setImageUrls((prev) => [...prev, url]);
+          setImages((prev) => [...prev, { url, name }]);
         });
       });
     });
   }, []);
 
-  // <input
-  //       type="file"
-  //       onChange={(event) => {
-  //         setImageUpload(event.target.files[0]);
-  //       }}
-  //     />
-  //     <button onClick={uploadFile}> Upload Image</button>
-
   return (
     <div className="App">
-      
-      {imageUrls.map((url) => {
-        return <img src={url} />;
-      })}
+      <button onClick={() => setShow(true)}>Dropdown</button>
+      {show ? (
+        <div className="dropdown-content">
+          {images.map((item, k) => {
+            const click = () => {
+              setShow(false);
+              setSelected(item);
+            };
+            return (
+              <button key={k} onClick={click}>
+                {item.name}
+              </button>
+            );
+          })}
+        </div>
+      ) : null}
+      {selected ? <img src={selected.url} alt="gif" /> : null}
     </div>
   );
 }
